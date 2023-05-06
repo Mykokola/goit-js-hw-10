f705755fc3b8c5fdc04d0c42fd7ad6779dc8c655
Mykokola <j0kfrbr@gmail.com>
2023-05-06 10:19:49 -0500

    f

---
+++ src/drawCountry.js
@@ -0,0 +1,51 @@
+import {fetchCountries} from './fetchCountries'
+export {fetchAndDrawCountries}
+const countryListEl = document.querySelector('.country-list');
+
+function getCauntryRow(country, showDetails) {
+    const languagesOneCauntry = Object.values(country.languages);
+    const header = `  <div style="
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  ">
+    <img style="
+    width: 30px;
+    height: 30px;
+  " src="${country.flags.svg}">
+    <h2>${country.name.common}</h2>
+    </div>`;
+    const details = showDetails
+      ? `<p>Capital:${country.capital[0]}</p>
+    <p>Population:${country.population}</p>
+    <p>Langauge:${languagesOneCauntry}</p>`
+      : ``;
+    return `<li>
+    ${header}
+    ${details}
+    </li>`;
+  }
+  function drawCountries(data) {
+    const showDetails = data.length == 1;
+    const countryList = data.map(e => getCauntryRow(e, showDetails));
+    countryListEl.insertAdjacentHTML('beforeend', countryList.join(''));
+  }
+  function fetchAndDrawCountries(name) {
+    fetchCountries(name)
+      .then(data => {
+        countryListEl.innerHTML = ``;
+        if (data.length >= 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (data.length) {
+          drawCountries(data);
+        }else{
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+        }
+      })
+      .catch(error => {
+        console.log('Cauntry is not a found')
+      });
+  }
+  
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import throttle from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries';
+import { fetchAndDrawCountries } from './drawCountry';
 const DEBOUNCE_DELAY = 300;
 const searchCauntryInput = document.querySelector('#search-box')
-    searchCauntryInput.addEventListener('input',throttle(() => fetchCountries(searchCauntryInput.value),300))
+    searchCauntryInput.addEventListener('input',throttle(() => fetchAndDrawCountries(searchCauntryInput.value),300))
    
